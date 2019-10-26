e2f669a15034b8d50d3525f54433b76e71a4b90d
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-10-26 18:08:10 -0500

    resolve variable names not matching

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nonLinearTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nonLinearTest.java
@@ -29,30 +29,30 @@ public class nonLinearTest extends teamMethods {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDriveFront  = hardwareMap.get(DcMotor.class, "left_drive_front");
-        leftDriveBack = hardwareMap.get(DcMotor.class, "left_drive_back");
-        rightDriveFront = hardwareMap.get(DcMotor.class, "right_drive_front");
-        rightDriveBack = hardwareMap.get(DcMotor.class, "right_drive_back");
+        motor1 = hardwareMap.get(DcMotor.class, "left_drive_front");
+        motor2 = hardwareMap.get(DcMotor.class, "left_drive_back");
+        motor3 = hardwareMap.get(DcMotor.class, "right_drive_front");
+        motor4 = hardwareMap.get(DcMotor.class, "right_drive_back");
         robotGyro = hardwareMap.get(GyroSensor.class,"gyrosensor");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDriveFront.setDirection(DcMotor.Direction.FORWARD);
-        leftDriveBack.setDirection(DcMotor.Direction.FORWARD);
-        rightDriveFront.setDirection(DcMotor.Direction.REVERSE);
-        rightDriveBack.setDirection(DcMotor.Direction.REVERSE);
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+        motor2.setDirection(DcMotor.Direction.FORWARD);
+        motor3.setDirection(DcMotor.Direction.REVERSE);
+        motor4.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Encoders and GyroSensor","Resetting");
-        leftDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDriveBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDriveBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robotGyro.calibrate();
 
-        leftDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDriveBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDriveBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor4.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
