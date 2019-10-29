6c3a0b9076d6c94f57da0f62b191185c666fd0f2
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-10-29 10:30:44 -0500

    Create Node.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathfinding/Node.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.pathfinding;
+
+public class Node {
+    String type = "";
+    double xpos;
+    double ypos;
+    double xsize;
+    double ysize;
+
+    Node(String typeI, double xposI, double yposI, double xsizeI, double ysizeI) {
+        xpos = xposI;
+        ypos = yposI;
+        xsize = xsizeI;
+        ysize = ysizeI;
+    }
+}
