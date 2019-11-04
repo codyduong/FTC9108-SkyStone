8942d6ddc32e1da03b838a928558f8c650c98238
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-11-04 12:59:48 -0600

    Create GameRegion.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathfinding/GameRegion.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.pathfinding;
+
+public class GameRegion {
+    double X_POS = 0;
+    double Y_POS = 0;
+
+    GameRegion(Node Node1,Node Node2, Node Node3, Node Node4) {
+        double X_MIN = Math.min(Math.min(Node1.X_POS, Node2.X_POS), Math.min(Node3.X_POS, Node4.X_POS));
+        double X_MAX = Math.max(Math.max(Node1.X_POS, Node2.X_POS), Math.max(Node3.X_POS, Node4.X_POS));
+        double Y_MIN = Math.min(Math.min(Node1.Y_POS, Node2.Y_POS), Math.min(Node3.Y_POS, Node4.Y_POS));
+        double Y_MAX = Math.min(Math.min(Node1.X_POS, Node2.X_POS), Math.min(Node3.X_POS, Node4.X_POS));
+
+    }
+
+
+}
