e9858dda5b9afe78bd058174417e8562187be1d2
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-11-13 12:02:17 -0600

    Resolved Issue #27

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_methods/DcMotorGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_methods/DcMotorGroup.java
@@ -21,7 +21,7 @@ public class DcMotorGroup {
     private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
     //Constructor
-    DcMotorGroup(DcMotor[] DcMotorArray) {
+    public DcMotorGroup(DcMotor[] DcMotorArray) {
         DcMotors = DcMotorArray;
         DcMotorCount = DcMotorArray.length;
     }
