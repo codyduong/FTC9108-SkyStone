4350174d50e69f61443137ba81fbecb9c4b8b367
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-11-17 00:52:03 -0600

    Resolves #44

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/Robot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.team_classes;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -8,6 +9,13 @@ public class Robot {
     public DcMotorGroup DCG;
     public ColorSensorGroup CSG;
     public BNOIMU IMU;
+
+    //constructor
+    public Robot() {
+        DCG = new DcMotorGroup(new DcMotor[]{null,null,null,null});
+        CSG = new ColorSensorGroup(null);
+        IMU = new BNOIMU(null);
+    }
 
     public void initialize(HardwareMap HM, Telemetry T) {
         DCG.initialize(HM, T);
