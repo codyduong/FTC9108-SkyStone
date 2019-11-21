c4615b28bef35710dafe528e485e915bd566abdd
Iain-Alderman <56171640+Iain-Alderman@users.noreply.github.com>
2019-11-21 11:47:05 -0600

    Open house adjustments  #2 / Organization
    I organized the code, and added code to make it park on the tape without color sensors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/ColorSensorGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/ColorSensorGroup.java
@@ -22,8 +22,8 @@ public class ColorSensorGroup {
     }
 
     public void initialize(HardwareMap Hmap, Telemetry Tm) {
-        ColorSensors[0] = Hmap.colorSensor.get("bottom_color");
-        ColorSensors[1] = Hmap.colorSensor.get("front_color");
+        //ColorSensors[0] = Hmap.colorSensor.get("bottom_color");
+        //ColorSensors[1] = Hmap.colorSensor.get("front_color");
         Tm.addData("DcMotorGroup Initialization","Complete");
         Tm.update();
     }
