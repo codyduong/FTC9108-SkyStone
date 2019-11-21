978edda7e77893defd26e15853cddd9afcd243d8
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-11-21 06:56:43 -0600

    v1.9.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_classes/Robot.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.team_classes;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
