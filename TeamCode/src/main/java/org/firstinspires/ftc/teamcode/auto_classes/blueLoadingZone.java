d8e51d28daff13479f6a8dcca52cbd4b883bc1fa
Iain-Alderman <56171640+Iain-Alderman@users.noreply.github.com>
2019-11-30 14:08:35 -0600

    Update blueLoadingZone.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/blueLoadingZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/blueLoadingZone.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.team_classes.Robot;
 
-@Autonomous(name="Loading Zone", group="Blue")
+@Autonomous(name="Blue Loading Zone", group="Blue")
 public class blueLoadingZone extends OpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
