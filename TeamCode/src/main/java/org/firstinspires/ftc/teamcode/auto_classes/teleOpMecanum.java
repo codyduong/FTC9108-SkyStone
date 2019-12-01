5f2a567239c04dcc99f91ddeef024e66f4724194
Cody-Duong <duongcody507@gmail.com>
2019-11-30 22:09:56 -0600

    v1.11.2p1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/teleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/teleOpMecanum.java
@@ -48,7 +48,7 @@ public class teleOpMecanum extends OpMode {
         if (Math.abs((double)turn) < .05) {
             turn = 0;
         }
-        double AverageHeading = (Math.toDegrees(Robot.IMU.getHeadingRadians()))/*/2*/;
+        double AverageHeading = (Math.toDegrees(Robot.IMU.getHeadingRadians()));
         Position2DAngle relativeValues = Robot.DCGm.relativeValues(new Position2DAngle(drivex,drivey,turn), AverageHeading);
         Robot.DCGm.driveToPositionAngle(relativeValues, true);
         telemetry.addData("Status", "Run Time: " + runtime.toString());
