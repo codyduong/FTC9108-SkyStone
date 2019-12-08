c6d2755600c25f49027d35e8cbdf9a5a7636bc49
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2019-12-08 13:57:36 -0600

    v1.12.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/drivers/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_classes/drivers/TeleOpMecanum.java
@@ -4,26 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.general_classes.Position2DAngle;
 import org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonAnalog;
-import org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonBinary;
 import org.firstinspires.ftc.teamcode.team_classes.driver_configuration.DriverConfiguration;
 import org.firstinspires.ftc.teamcode.team_classes.robot.Robot;
 
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Analog_Action.drivex;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Analog_Action.drivey;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Analog_Action.turn;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.faceDown;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.faceLeft;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.faceRight;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.faceUp;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.resetGyro;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.swapDriveMode;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.turnLeft;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.turnRight;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonAnalog.Analog.left_stick_x;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonAnalog.Analog.left_stick_y;
-import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonAnalog.Analog.right_stick_y;
+import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonAnalog.Analog.*;
+import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Analog_Action.*;
+import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.ButtonBinary.Binary.*;
+import static org.firstinspires.ftc.teamcode.team_classes.driver_configuration.Action.Binary_Action.*;
 
 @TeleOp(name="Default Normal", group="9108") //fix this
 public class TeleOpMecanum extends OpMode {
@@ -40,18 +28,18 @@ public class TeleOpMecanum extends OpMode {
         Robot.Driver1 = new DriverConfiguration(Robot, gamepad1);
         Robot.Driver2 = new DriverConfiguration(Robot, gamepad2);
         Robot.Driver1.assignDebounce(500);
-        Robot.Driver1.assignAnalog(                         left_stick_x, drivex);
-        Robot.Driver1.assignAnalog(                         left_stick_y, drivey);
-        Robot.Driver1.assignSign(                           left_stick_y, ButtonAnalog.SIGN.NEGATIVE);
-        Robot.Driver1.assignAnalog(                         right_stick_y, turn);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     y, swapDriveMode);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     dpad_up, faceUp);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     dpad_right, faceRight);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     dpad_down, faceDown);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     dpad_left, faceLeft);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     left_bumper, turnLeft);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     right_bumper, turnRight);
-        Robot.Driver1.assignBinary(ButtonBinary.Binary.     back, resetGyro);
+        Robot.Driver1.assignAnalog(left_stick_x, drivex);
+        Robot.Driver1.assignAnalog(left_stick_y, drivey);
+        Robot.Driver1.assignSign(left_stick_y, ButtonAnalog.SIGN.NEGATIVE);
+        Robot.Driver1.assignAnalog(right_stick_y, turn);
+        Robot.Driver1.assignBinary(y, swapDriveMode);
+        Robot.Driver1.assignBinary(dpad_up, faceUp);
+        Robot.Driver1.assignBinary(dpad_right, faceRight);
+        Robot.Driver1.assignBinary(dpad_down, faceDown);
+        Robot.Driver1.assignBinary(dpad_left, faceLeft);
+        Robot.Driver1.assignBinary(left_bumper, turnLeft);
+        Robot.Driver1.assignBinary(right_bumper, turnRight);
+        Robot.Driver1.assignBinary(back, resetGyro);
         Robot.DCGm.setRelativeDrive(true);
         Robot.RHG.Hubs[0].setLedColor(255,255,255);
         telemetry.addData("Status","Complete");
